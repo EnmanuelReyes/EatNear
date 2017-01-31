0f762b16608f1bd631ea6e114fe544a53f4f7074
Enmanuel Reyes <enmanuelreyes@live.com>
2017-01-30 22:34:51 -0400

    Configuring OAuth 2

--- src/main/java/me/enmanuel/eatnear/config/WebSecurityConfiguration.java
+++ src/main/java/me/enmanuel/eatnear/config/WebSecurityConfiguration.java
@@ -1,8 +1,25 @@
 package me.enmanuel.eatnear.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,20 +28,70 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * Time: 10:03 AM
  */
 @Configuration
+@EnableAutoConfiguration
+@RestController
 public class WebSecurityConfiguration extends
         WebSecurityConfigurerAdapter {
+
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("enma").password("koko").roles("USER");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
-//        http_0_, user0_.last_name as last_nam2_0_, user0_.name as n
-//                .formLogin()
-//                .loginPage("/login")
-//                .defaultSuccessUrl("/profile")
-//                .and()
-//                .logout().logoutSuccessUrl("/login")
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/webjars/**", "/login","/signin/**","/signup").permitAll()
-//                .anyRequest().authenticated();
+        http.authorizeRequests().antMatchers("/swagger-ui.html").permitAll().anyRequest().authenticated().and().csrf().disable();
     }
+
+    @EnableAuthorizationServer
+    @Configuration
+    protected static class ResourceServer extends AuthorizationServerConfigurerAdapter {
+        @Autowired
+        private AuthenticationManager auth;
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory().withClient("acme").secret("acmesecret").authorizedGrantTypes("password")
+                    .accessTokenValiditySeconds(1000).scopes("openid");
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+                throws Exception {
+            endpoints.authenticationManager(auth);
+        }
+    }
+
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends
+            ResourceServerConfigurerAdapter {
+
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                    .authorizeRequests()
+                    .antMatchers("/swagger-ui.html").permitAll()
+                    .antMatchers("/api/**").hasRole("USER").anyRequest().permitAll();
+            // @formatter:on
+        }
+
+    }
+
+
+    @Autowired
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        // @formatter:off
+        auth.inMemoryAuthentication().withUser("enma").password("nana").roles("USER");
+        // @formatter:on
+    }
+
 }
